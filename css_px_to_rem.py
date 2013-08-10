c8a2d6c3a20dabb5b3dab452e736f6c4a307bc12
Marco Bonetti <mbonetti@gmail.com>
2013-08-10 14:27:07 +0200

    expand the selection to the current line if the selection is empty

--- css_px_to_rem.py
+++ css_px_to_rem.py
@@ -12,7 +12,7 @@ class PxToRemCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         for s in self.view.sel():
             if s.empty():
-                s = self.view.word(s)
+                s = self.view.line(s)
 
             txt = self.transform(self.view.substr(s))
             self.view.replace(edit, s, txt)
@@ -21,8 +21,8 @@ class PxToRemCommand(sublime_plugin.TextCommand):
         def repl(m):
             gd = m.groupdict()
             if gd.get('unit', None) == 'px':
-                return '%3.4frem%s' % (int(gd.get('qty')) / float(self.rem_height), gd.get('foll'))
+                return '%3.4frem' % (int(float(gd.get('qty'))) / float(self.rem_height))
             if gd.get('unit', None) == 'rem':
-                return '%dpx%s' % (int(float(gd.get('qty')) * float(self.rem_height)), gd.get('foll'))
+                return  unicode(int(float(gd.get('qty')) * float(self.rem_height))) + u'px'
 
-        return re.sub(r'((?P<qty>[\d\.]+)\s*(?P<unit>px|rem)(?P<foll>[\s;]))', repl, str)
+        return re.sub(r'((?P<qty>[0-9\.]+)(?P<unit>px|rem))', repl, str)
